a9e8bc0af9abcaa056e3755ebe9afd9749dd86e2
imranertaza <imranertaza12@gmail.com>
2024-10-26 18:15:54 +0600

    git commit -m " Subscribe Newsletter verify Co-authored-by: Murad <dnationsoftdm8@gmail.com>"

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -11,6 +11,7 @@ class Home extends BaseController {
     {
         $this->validation = \Config\Services::validation();
         $this->session = \Config\Services::session();
+        $this->encrypter = \Config\Services::encrypter();
     }
 
     /**
@@ -125,35 +126,25 @@ class Home extends BaseController {
         $email = $this->request->getPost('email');
 
         if (!empty($email)){
-            if(is_exists('cc_newsletter','email',$email) == true) {
-                $encrypt_method = "AES-256-CBC";
-                $secret_key = 'AA74CDCC2BBRT935136HH7B63C27'; // user define private key
-                $secret_iv = '5fgf5HJ5g27'; // user define secret key
-                $key = hash('sha256', $secret_key);
-                $iv = substr(hash('sha256', $secret_iv), 0, 16);
 
+            $name = get_lebel_by_value_in_settings('store_name');
+            $otp = rand(100000,999999);
+            $url = base_url('user_subscribe_verify?email='.urlencode($this->encrypter->encrypt($email)).'&code='.urlencode($this->encrypter->encrypt($otp)));
+            $subject = 'Please Verify Your Email Address to Complete Your Subscription!';
+            $message = "Thank you for subscribing to ".$name."! Before we can start sending you our updates, we just need to confirm your email address.<br>                    
+                Please verify your email by clicking the link below: <a href='".$url."'>Verify My Email Address</a><br>                    
+                If you did not sign up for this subscription, please disregard this email.<br>                    
+                Thank you for choosing ".$name."!";
 
-                $name = get_lebel_by_value_in_settings('store_name');
-                $otp = rand(100000,999999);
-                $url = base_url('user_subscribe_verify?email='.openssl_encrypt($email,$encrypt_method, $key, 0, $iv).'&code='.openssl_encrypt($otp,$encrypt_method, $key, 0, $iv));
-                $subject = 'Please Verify Your Email Address to Complete Your Subscription!';
-                $message = "Thank you for subscribing to ".$name."! Before we can start sending you our updates, we just need to confirm your email address.<br>                    
-                    Please verify your email by clicking the link below: <a href='".$url."'>Verify My Email Address</a><br>                    
-                    If you did not sign up for this subscription, please disregard this email.<br>                    
-                    Thank you for choosing ".$name."!";
+            $sessionArray = [
+                'otp' => $otp,
+                'email' => $email,
+            ];
+            $this->session->set($sessionArray);
 
-                $sessionArray = [
-                    'otp' => $otp,
-                    'email' => $email,
-                ];
-                $this->session->set($sessionArray);
+            email_send($email,$subject,$message);
+            print "Please Verify Your Email Address to Complete Your Subscription!";
 
-                email_send($email,$subject,$message);
-
-                print "Please Verify Your Email Address to Complete Your Subscription!";
-            }else{
-                print 'Your email already exists';
-            }
         }else{
             print 'Email required';
         }
@@ -161,29 +152,29 @@ class Home extends BaseController {
 
     public function verify(){
 
-        $encrypt_method = "AES-256-CBC";
-        $secret_key = 'AA74CDCC2BBRT935136HH7B63C27'; // user define private key
-        $secret_iv = '5fgf5HJ5g27'; // user define secret key
-        $key = hash('sha256', $secret_key);
-        $iv = substr(hash('sha256', $secret_iv), 0, 16);
-
-
         $email = $this->request->getGetPost('email');
         $code = $this->request->getGetPost('code');
+        if(!empty($email)) {
+            $email_decrypt = $this->encrypter->decrypt($email);
+            $otp_decrypt = $this->encrypter->decrypt($code);
 
-        $email_d =  openssl_decrypt($email,$encrypt_method, $key, 0, $iv);
-        $otp_d = openssl_decrypt($code,$encrypt_method, $key, 0, $iv);
+            if (($email_decrypt == $this->session->email) && ($otp_decrypt == $this->session->otp)) {
+                $newData['email'] = $email_decrypt;
 
-        if (($email_d == $this->session->email) && ($otp_d == $this->session->otp)){
-            $newData['email'] = $email_d;
-            $newAd = DB()->table('cc_newsletter');
-            $newAd->insert($newData);
+                if(is_exists('cc_newsletter','email',$email_decrypt) == false) {
+                    $newAd = DB()->table('cc_newsletter');
+                    $newAd->insert($newData);
+                }
 
-            setcookie('down_image',$email_d,time()+ (86400 * 365), "/");
-            $this->session->setFlashdata('message', '<div class="alert-success_web py-2 px-3 border-0 text-white fs-5 text-capitalize" role="alert">Subscribe successfully completed </div>');
-            return redirect()->to('/');
+                setcookie('download_image', $email_decrypt, time() + (86400 * 365), "/");
+                $this->session->setFlashdata('message', '<div class="alert-success_web py-2 px-3 border-0 text-white fs-5 text-capitalize" role="alert">Subscribe successfully completed </div>');
+                return redirect()->to('/');
+            } else {
+                $this->session->setFlashdata('message', '<div class="alert-success_web py-2 px-3 border-0 text-white fs-5 text-capitalize" role="alert">Information not matching </div>');
+                return redirect()->to('/');
+            }
         }else{
-            $this->session->setFlashdata('message', '<div class="alert-success_web py-2 px-3 border-0 text-white fs-5 text-capitalize" role="alert">Something went wrong please try again! </div>');
+            $this->session->setFlashdata('message', '<div class="alert-success_web py-2 px-3 border-0 text-white fs-5 text-capitalize" role="alert">Information not matching </div>');
             return redirect()->to('/');
         }
 
